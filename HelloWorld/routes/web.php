48f9bd2b55a3285ec2e961dfdeba78a259eba849
Braedon Niemi <70549370+btniemi@users.noreply.github.com>
2021-02-19 10:20:17 -0700

    work on project

--- HelloWorld/routes/web.php
+++ HelloWorld/routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get("/talk", function() {
+    return view('talk');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
---
+++ ListBuilder/database/migrations/2021_02_19_164706_create_build_lists_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBuildListsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('build_lists', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('list_type');
+            $table->string('item');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('build_lists');
+    }
+}
